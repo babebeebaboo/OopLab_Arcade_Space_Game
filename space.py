a5ae0cc476bf38b8190ff3aedb3d2ffb0c2e3f42
BABE <supakorn.wo@ku.th>
2017-09-08 15:09:55 +0700

    add ship world

--- space.py
+++ space.py
@@ -1,30 +1,24 @@
 import arcade
-from models import Ship
+from models import Ship,World
  
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
-
 
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
  
         arcade.set_background_color(arcade.color.BLACK)
- 
-        self.ship = Ship(100,100)
         self.ship_sprite = arcade.Sprite('images/ship.png')
- 
- 
+        self.world = World(width, height)        # แทนที่บรรทัด ship
+
     def on_draw(self):
         arcade.start_render()
         self.ship_sprite.draw()
  
     def update(self, delta):
-        ship = self.ship
- 
-        ship.update(delta)
-        self.ship_sprite.set_position(ship.x, ship.y)
-        
+        self.world.update(delta)
+        self.ship_sprite.set_position(self.world.ship.x, self.world.ship.y)
 
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
