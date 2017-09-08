0c2a73b8d1fe3449b2e319be5b17af5b7b13d659
BABE <supakorn.wo@ku.th>
2017-09-08 14:37:51 +0700

    add Sprite

--- space.py
+++ space.py
@@ -2,19 +2,18 @@ import arcade
  
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
- 
+
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
- 
+
         arcade.set_background_color(arcade.color.BLACK)
- 
- 
+        self.ship = arcade.Sprite('images/ship.png')
+        self.ship.set_position(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+        
     def on_draw(self):
         arcade.start_render()
-
-        
- 
+        self.ship.draw()
  
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
